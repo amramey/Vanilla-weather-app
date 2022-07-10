9c946ebde58627c2a89ad6cd9725b21bfa29b1e8
amramey <104532175+amramey@users.noreply.github.com>
2022-07-10 13:59:59 -0700

    removed any console.log displayed

--- app.js
+++ app.js
@@ -70,10 +70,9 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "137febab6c9d81eb50e859bf6a2bd5de";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 
